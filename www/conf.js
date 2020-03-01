cad01e11d5b7acbe82e99ce4a6fe31c9ebe9ea9c
popbody <58682722+popbody@users.noreply.github.com>
2020-03-01 18:46:35 +0800

    Update conf.js

--- www/conf.js
+++ www/conf.js
@@ -27,7 +27,7 @@ jsproxy_config({
        
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        '608.qzsdy.workers.dev': 1,
+        '608.qzsdy.workers.dev': 4,
         }
     }
   },
